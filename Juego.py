7afa369ba85dfbf752425259dce8c10e8a0524e2
AndreaChavez1997 <anchavezol@uide.edu.ec>
2025-12-18 15:27:42 -0500

    Refactor Piedra, Papel o Tijera game logic
    Se realiza la modificación necesaria para el funcionamiento del programa. El programa se encuentra funcional

--- Juego.py
+++ Juego.py
@@ -1,21 +1,46 @@
+#Genera aleatoriamente
 import random
-#Se genera aleatoriamente
-opciones = ["piedra", "papel", "tijera"]
-#variables
-print("BIENVENIDOS AL JUEGO\n")
-print("Ingresa tu nombre\n")
-    
-   print(f"Escoje piedra, papel o tijera: {opciones}")
-    jugador = input("Elige piedra, papel o tijera): ").lower()
-    #print(f"La computadora eligió: {computadora}")
-    computadora = random.choice(opciones)
-    if jugador == computadora:
+print(" Bienvenidos a Piedra, Papel o Tijera ")
+juego = True
+#Iniciamos un bucle para que el juego continúe o se detenga
+while juego:
+    #Creamos una función
+    def jugar():
+        #Se crea una lista de los elementos del juego
+        opc = ["piedra", "papel", "tijera"]
+        nombre = input("Por favor escribe tu nombre: ")
+        #Se solicita el nombre para que sea personalizado
+        #Se utiliza la funcion lower para que internamente las letras se conviertan en 
+        #minúsculas y no salte error
+        usuario = input(f"{nombre} elige una opción (piedra, papel, tijera): \n").lower()
+        #Condicional en caso de escribir algo diferente a las opciones
+        if usuario not in opc:
+            print("Opción no válida. Inténtalo de nuevo.")
+            return
+        
+        #Almacena en una variable el elemento aleatorio del programa
+        sistema = random.choice(opc)
+        print("Piedra, papel o tijera 1,2 y 3!\n")
+        print(f"Tu elegiste:       {usuario}")
+        print(f"El sistema eligió: {sistema}\n")
+        #Compara la selección del usuario y la máquina
+        if usuario == sistema:
+            #En caso de que sean iguales
+            print("Ninguno nada, es empate")
+            
+        elif (sistema == "papel" and usuario == "tijera") or \
+             (sistema == "tijera" and usuario == "piedra") or \
+             (sistema == "piedra" and usuario == "papel"):
+            print("¡Ganaste!")
+        else:
+            #Si no cumple con ninguna de las condiciones
+            print("Perdiste. Inténtalo de nuevo.")
 
-        print("¡Juega otra vez!\n")
-    elif (
-        (jugador == "piedra" and computadora == "tijera")
-     
-    ):
-        print("¡Ganaste!\n")
-    else:
-         (jugador == "papel" and computadora == "piedra") 
+    if __name__ == "__main__":
+        #Se almacena la función
+        jugar()
+        #Parte final del bucle
+    respuesta = input("¿Quieres jugar nuevamente? (s/n): \n").lower()
+    if respuesta != 's':
+        juego = False
+        print("Gracias por visitarnos. Vuelve pronto para jugar")
